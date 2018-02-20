40c2c0d4ef773e3a057bc16e9cfaf58125f92842
Peter Cooper <pcooper19@gmail.com>
2018-02-20 11:17:40 -0500

    Pseudocode architecture

---
+++ slicer.py
@@ -0,0 +1,11 @@
+# get user e-mail address
+
+email = input("What is your e-mail address?: ").strip()
+
+# slice out username
+
+# slice out domain name
+
+# format output message
+
+# display output message
