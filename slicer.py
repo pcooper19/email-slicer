10b81845b8393b1d362fd57661665e66e75df799
Peter Cooper <pcooper19@gmail.com>
2018-02-20 11:26:49 -0500

    string project finished

--- slicer.py
+++ slicer.py
@@ -4,8 +4,16 @@ email = input("What is your e-mail address?: ").strip()
 
 # slice out username
 
+user = email[:email.index("@")]
+
 # slice out domain name
+
+domain = email[email.index("@") + 1:]
 
 # format output message
 
+output = "Your username is {} and your domain name is {}".format(user,domain)
+
 # display output message
+
+print (output)
